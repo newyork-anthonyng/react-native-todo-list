ea520139c2858c9aa7a32e81d6b92bc563320b84
Anthony Ng <newyork.anthonyng@gmail.com>
2021-02-11 21:07:57 -0500

    add fetch call

--- App.js
+++ App.js
@@ -1,11 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, View, FlatList, Switch, Modal, Button } from "react-native";
+
+/*
+ * {
+    "userId": 1,
+    "id": 1,
+    "title": "delectus aut autem",
+    "completed": false
+   },
+*/
+function fetchTodos() {
+  return fetch('https://jsonplaceholder.typicode.com/todos')
+    .then(response => response.json())
+}
+
+function TodoItem({ title, completed }) {
+  return (
+    <View>
+      <Text>{title}</Text>
+      <Switch value={completed} />
+    </View>
+  );
+}
 
 export default function App() {
+  const [todos, setTodos] = React.useState([]);
+
+  useEffect(() => {
+    fetchTodos()
+      .then(setTodos);
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Button
+        onPress={() => alert('Hello World')}
+        title="Add new todo item"
+      />
+      <FlatList
+        data={todos}
+        renderItem={({ item }) => {
+          return <TodoItem {...item} />
+        }}
+        keyExtractor={item => item.id}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +53,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
